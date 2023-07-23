3534d90cf5dbc7903c14960bdcb76c67e4a02ef0
Aditya Prakash <AdityaPrakash@MacBook-Pro-M1.local>
2023-07-23 15:47:22 +0530

    adding changes

---
+++ LoggerUsingchainOfResp/Program.cs
@@ -0,0 +1,8 @@
+﻿LoggerChainBuilder builder = new LoggerChainBuilder();
+Logger logger = builder.Build();
+
+// Test the logger
+logger.Log("This is an informational message.", LogLevel.INFO);
+logger.Log("This is a warning message.", LogLevel.WARNING);
+logger.Log("This is an error message.", LogLevel.ERROR);
+logger.Log("This is a fatal message.", LogLevel.FATAL);
